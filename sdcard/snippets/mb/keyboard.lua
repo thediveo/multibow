e62928c61ca4aba7c9585e18ef5418af68aeb6da
TheDiveO <thediveo@gmx.eu>
2019-04-03 21:54:00 +0200

    wip

--- sdcard/snippets/mb/keyboard.lua
+++ sdcard/snippets/mb/keyboard.lua
@@ -2,9 +2,9 @@
 -- List of modifier levels in the keymaps, and their associated modifier key(s).
 local keymap_levels = {
     {field='norm', mods=0},
-    {field='shift', mods=keybow.LEFT_SHIFT},
-    {field='altgr', mods=keybow.RIGHT_ALT},
-    -- TODO: altgrshift
+    {field='shift', mods=1<<keybow.LEFT_SHIFT},
+    {field='altgr', mods=1<<keybow.RIGHT_ALT},
+    {field='altgrshift', mods=(1<<keybow.RIGHT_ALT)+(1<<keybow.LEFT_SHIFT)}
 }
 
 -- Calculates a map for quickly mapping glyphs to keycodes and modifier keys.
@@ -18,9 +18,10 @@ function create_map(layout)
     -- in order to build a map for quickly translating individual glyphs into
     -- their USB keycodes and modifier(s).
     for _, kl in ipairs(keymap_levels) do
+        field = kl.field
         mods = kl.mods
         for row = 1, 4 do
-            for col, glyph in ipairs(layout[kl.field][row]) do
+            for col, glyph in ipairs(layout[field][row]) do
                 map[glyph] = {
                     keycode=kb.keycodes_layout[row][col],
                     mods=mods
@@ -30,4 +31,12 @@ function create_map(layout)
     end
     -- Store the freshly built map in the specific layout for later quick use.
     layout.map = map
+end
+
+function kb.set_layout(code)
+    l = kb[code]
+    if l then
+        create_map(l)
+        kb.layout = l
+    end
 end
