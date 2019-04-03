79427cb2b62e58d18d1567303ac872fe18ab5004
TheDiveO <thediveo@gmx.eu>
2019-04-03 22:08:48 +0200

    wip

--- sdcard/snippets/mb/keyboard.lua
+++ sdcard/snippets/mb/keyboard.lua
@@ -26,7 +26,7 @@ function create_map(layout)
                     keycode=kb.keycodes_layout[row][col],
                     mods=mods
                 }
-            end 
+            end
         end
     end
     -- Store the freshly built map in the specific layout for later quick use.
@@ -38,5 +38,27 @@ function kb.set_layout(code)
     if l then
         create_map(l)
         kb.layout = l
+    end
+end
+
+
+-- Please note: since Lua hasn't really any Unicode support, despite fishy
+-- claims to the contrary, we need to be careful when processing Unicode strings
+-- (in UTF-8 encoding). Multibow users must encode their scripts in UTF-8. This
+-- way, we can split such UTF-8 string correctly at glyph boundaries in order to
+-- translate Unicode glyphs into their corresponding keystrokes, if available.
+-- Plain ASCII wouldn't get us too far here when it comes to international
+-- keyboards. Thus, we work with Unicode in UTF-8 encoding.
+function kb.string_to_keycodes(s)
+    -- See https://stackoverflow.com/a/13238257 for breaking an UTF-8 string
+    -- down into its individual glyphs (also in UTF-8), thus breaking the Lua
+    -- binary string at glyph boundaries. This assumes that we got valid UTF-8
+    -- encoding at this point, which is a pretty safe bet.
+    for glyph in str:gmatch("[%z\1-\127\194-\244][\128-\191]*") do
+        -- translate glyph (iff known)
+        keycode = kb.layout.map[glyph]
+        if keycode then
+            -- ...
+        end
     end
 end
